92c878f57c241e14f05ab7c9ba1dd3f37c56799e
Setiady Wiguna <setiady@SW-MacBook-Pro.local>
2016-06-30 11:01:01 +0700

    update tabbar assets

--- icaew/icaew/Controllers/BaseTabBarController.swift
+++ icaew/icaew/Controllers/BaseTabBarController.swift
@@ -32,39 +32,41 @@ class BaseTabBarController: UITabBarController {
         
         let tabBarController = BaseTabBarController()
         tabBarController.tabBar.translucent = false
+        tabBarController.tabBar.tintColor = UIColor.whiteColor()
+        tabBarController.tabBar.barTintColor = UIColor.blackColor()
         tabBarController.viewControllers = [programmesNavigationController,
                                             eventsNavigationController,
                                             universityNavigationController,
                                             contactNavigationController,
                                             aboutNavigationController]
         
-        let titleTextAttributes = [NSFontAttributeName : UIFont.systemFontOfSize(10)];
+        let titleTextAttributes = [NSFontAttributeName : UIFont.systemFontOfSize(9)];
         let titlePosition = UIOffsetMake(0, -3)
         
         var tabBarItems = tabBarController.tabBar.items as [UITabBarItem]!
-        tabBarItems[0].image = UIImage(named: "tab-events-icon")
+        tabBarItems[0].image = UIImage(named: "tabicon-programmes")
         tabBarItems[0].imageInsets = UIEdgeInsetsMake(0, -0.5, 0, 0.5)
         tabBarItems[0].title = "Programmes"
         tabBarItems[0].titlePositionAdjustment = titlePosition
         tabBarItems[0].setTitleTextAttributes(titleTextAttributes, forState: .Normal)
         
-        tabBarItems[1].image = UIImage(named: "tab-social-icon")
+        tabBarItems[1].image = UIImage(named: "tabicon-event")
         tabBarItems[1].imageInsets = UIEdgeInsetsMake(0, -2, 0, 2)
         tabBarItems[1].title = "Events"
         tabBarItems[1].titlePositionAdjustment = titlePosition
         tabBarItems[1].setTitleTextAttributes(titleTextAttributes, forState: .Normal)
         
-        tabBarItems[2].image = UIImage(named: "tab-chat-icon")
+        tabBarItems[2].image = UIImage(named: "tabicon-universities")
         tabBarItems[2].title = "Universities"
         tabBarItems[2].titlePositionAdjustment = titlePosition
         tabBarItems[2].setTitleTextAttributes(titleTextAttributes, forState: .Normal)
         
-        tabBarItems[3].image = UIImage(named: "tab-more-icon")
+        tabBarItems[3].image = UIImage(named: "tabicon-about-us")
         tabBarItems[3].title = "About Us"
         tabBarItems[3].titlePositionAdjustment = titlePosition
         tabBarItems[3].setTitleTextAttributes(titleTextAttributes, forState: .Normal)
         
-        tabBarItems[4].image = UIImage(named: "tab-more-icon")
+        tabBarItems[4].image = UIImage(named: "tabicon-contact-us")
         tabBarItems[4].title = "Contact Us"
         tabBarItems[4].titlePositionAdjustment = titlePosition
         tabBarItems[4].setTitleTextAttributes(titleTextAttributes, forState: .Normal)
