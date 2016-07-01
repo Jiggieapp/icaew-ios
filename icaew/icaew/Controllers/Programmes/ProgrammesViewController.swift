379ace53205be0568c3982cc3e633f7d0a05d386
uuds <nuruddin@jiggieapp.com>
2016-07-01 17:58:17 +0700

    Add Programmes page.

--- icaew/icaew/Controllers/Programmes/ProgrammesViewController.swift
+++ icaew/icaew/Controllers/Programmes/ProgrammesViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class ProgrammesViewController: BaseViewController {
+private let kProgrammesCellIdentifier = "kProgrammesCellIdentifier"
+private let kTableHeaderViewHeight: CGFloat = 213
 
+class ProgrammesViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView!
+    
+    private var headerImageView: UIImageView?
+    private var headerView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.hideNavigationBar()
+        self.setupView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +31,77 @@ class ProgrammesViewController: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: View
+    private func setupView() {
+        self.headerView = UIView(frame: CGRectMake(0, 0, UIScreen.width(), kTableHeaderViewHeight))
+        self.headerImageView = UIImageView(image: UIImage(named: "image-home")!)
+        
+        if let headerImageView = self.headerImageView {
+            self.headerView.addSubview(headerImageView)
+            
+            headerImageView.contentMode = .ScaleAspectFill
+            headerImageView.clipsToBounds = true
+            self.headerView.clipsToBounds = true
+        }
+        
+        self.tableView.tableHeaderView = self.headerView
+        self.tableView.tableFooterView = UIView()
+        self.tableView.registerNib(ProgrammesTableViewCell.nib(),
+                                   forCellReuseIdentifier: kProgrammesCellIdentifier)
     }
-    */
+    
+    // MARK: UITableViewDataSource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(kProgrammesCellIdentifier) as! ProgrammesTableViewCell
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.001
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    // MARK: UIScrollViewDelegate
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if self.headerView.clipsToBounds {
+            self.headerView.clipsToBounds = false
+        }
+        
+        if let headerImageView = self.headerImageView {
+            let yPos = -scrollView.contentOffset.y
+            
+            if yPos > 0 {
+                var imageViewFrame = headerImageView.frame
+                imageViewFrame.origin.y = scrollView.contentOffset.y
+                imageViewFrame.size.height = kTableHeaderViewHeight+yPos
+                headerImageView.frame = imageViewFrame
+            }
+        }
+    }
 
 }
