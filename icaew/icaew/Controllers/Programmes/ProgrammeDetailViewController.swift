c02f10d71126df59f4277f3aee03f5530aadc3a6
Setiady Wiguna <setiady@SW-MacBook-Pro.local>
2016-07-28 14:45:40 +0700

    make label in programmes data support html tags

--- icaew/icaew/Controllers/Programmes/ProgrammeDetailViewController.swift
+++ icaew/icaew/Controllers/Programmes/ProgrammeDetailViewController.swift
@@ -29,6 +29,23 @@ class ProgrammeDetailViewController: BaseViewController, MFMailComposeViewContro
         let videoId = self.programme.youtubeURL.componentsSeparatedByString("watch?v=").last!
         
         self.detailLabel.text = self.programme.detail
+        
+        var description = programme.detail
+        description += "<style>body{font-family: '\(self.detailLabel.font.fontName)'; font-size: \(self.detailLabel.font.pointSize)px; color: #000000;}</style>"
+        
+        if let htmlData = description.dataUsingEncoding(NSUnicodeStringEncoding) {
+            do {
+                let attributedText = try NSAttributedString(data: htmlData, options: [NSDocumentTypeDocumentAttribute : NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute : NSUTF8StringEncoding], documentAttributes: nil)
+                self.detailLabel.text = nil
+                self.detailLabel.attributedText = attributedText
+            } catch let error {
+                print("Couldn't translate \(description): \(error) ")
+            }
+        } else {
+            self.detailLabel.attributedText = nil
+            self.detailLabel.text = description
+        }
+        
         self.playerView.loadWithVideoId(videoId)
     }
     
